763d8a40bd89d87447b25346d5b52db740d5f4be
Leon <96251621+Leon0b1101@users.noreply.github.com>
2022-09-18 17:33:03 +0200

    first commit

---
+++ todoapp/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+// @ts-ignore
+import { onMount } from "svelte";
+  let todos = [];
+  let todoText = '';
+
+  onMount(() => {
+    const existingTodos = localStorage.getItem('todos');
+  });
+
+  function newTodo() {
+    todos = [...todos, todoText];
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
+
+
+</script>
+
+<main>
+  <ul>
+    {#each todos as todo}
+    <li>{todo}</li>
+    {/each}
+  </ul>
+  <form on:submit|preventDefault={newTodo}>
+  <input bind:value={todoText}>
+  <input type="submit" value="New Todo...">
+  </form>
+</main>
+
+<style>
+
+</style>
